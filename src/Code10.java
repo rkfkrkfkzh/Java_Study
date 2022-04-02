dd30f0e6b8a912b1fcf41fb29756b040f38f7a93
imhyojin <1004dlagywls@naver.com>
2022-04-02 23:33:10 +0900

    Java로 배우는 자료구조 변수,배열,반복문

--- src/Code10.java
+++ src/Code10.java
@@ -5,6 +5,9 @@ public class Code10 {
 
         boolean isPrime = true;
         for (int i = 2; i <= n / 2; i++) {
+//            각각의 정수 n에 대해서 for문을 돌면서 2보다 크거나 같은 약수가 있는지 검사한다.
+//            하나라도 약수가 있다면 이미 소수가 아니므로 더이상 검사할 필요가 없다.
+//            뱐스 isPrime이 어떤 역할을 하는지 생각해보자
             if (n % i == 0)
                 isPrime = false;
 
